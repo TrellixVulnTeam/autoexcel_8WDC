0260bec85f08af188451712a70ab36afc3d0c9ae
khanhnd185 <khanhdangnguyen@rvc.renesas.com>
2018-12-03 11:08:56 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+For those who cannot install openpyxl by pip because of security system
+I refer to this website: http://www.python-excel.org/
---
+++ example.py
@@ -0,0 +1,10 @@
+# Example
+
+import os
+os.getcwd()
+os.chdir('.\et_xmlfile-1.0.1')
+import et_xmlfile
+os.chdir('..\jdcal-1.4')
+import jdcal
+os.chdir('..\openpyxl')
+import openpyxl
